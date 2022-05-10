a88495332d3119043d4fe9af92fb96acca5bdd67
sakura <320462273@qq.com>
2022-05-10 14:46:13 +0800

    first commit

---
+++ rbac/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class User(models.Model):
+    name = models.CharField(max_length=32)
+    pwd = models.CharField(max_length=32)
+    roles = models.ManyToManyField(to="Role")
+
+    def __str__(self): return self.name
+
+
+class Role(models.Model):
+    title = models.CharField(max_length=32)
+    permissions = models.ManyToManyField(to="Permission")
+
+    def __str__(self): return self.title
+
+
+class Permission(models.Model):
+    title = models.CharField(max_length=32)
+    url = models.CharField(max_length=1024)
+    action = models.CharField(max_length=32, default="")
+    group = models.ForeignKey("PermissionGroup", default=1, on_delete=models.CASCADE)
+
+    def __str__(self): return self.title
+
+
+class PermissionGroup(models.Model):
+    title = models.CharField(max_length=32)
+
+    def __str__(self): return self.title
